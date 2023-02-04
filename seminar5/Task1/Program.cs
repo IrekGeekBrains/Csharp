e6c9ae5e500d299273a41622056426392eb56362
Irek <irek.sh.r@gmail.com>
2023-02-04 13:13:24 +0400

    add task1

---
+++ seminar5/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] GenerateArray(int len)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int Find(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int[] array = GenerateArray(10);
+PrintArray(array);
+Console.WriteLine($"Четных чисел в массиве: {Find(array)}");
