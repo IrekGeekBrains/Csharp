e6f8e4ec9318f003375f8d6d16529ff2e0ff2bb8
Irek <irek.sh.r@gmail.com>
2023-02-05 13:06:39 +0400

    add task1

---
+++ seminar6/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int[] Array(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = Prompt("Введите число: ");
+    }
+    return array;
+}
+
+int Find(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int number = Prompt("Введите сколько чисел нужно проверить: ");
+// int [] array = Array(number);
+System.Console.WriteLine($"Положительных чисел, введенных пользователем, = {Find(Array(number))}");
