aef3c1071fa04a7fe2f685f9cfa39191f762da84
Irek <irek.sh.r@gmail.com>
2023-02-19 23:32:03 +0400

    add task1-3

---
+++ seminar9/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Numbers(int m, int n)
+{
+    if (m > n)
+    {
+        return 0;
+    }
+
+    return Numbers((m + 1), n) + m;
+
+}
+
+int m = Prompt("Введите число M");
+int n = Prompt("Введите число N");
+
+if (m<n)
+{
+System.Console.WriteLine(Numbers(m, n));
+}
+else
+{
+    System.Console.WriteLine("Ошибка. M должно быть меньше N");
+}
+
